cc0553e5a4d21dc63289193f2edfe4d24c5191ff
BIG-MON <mrmon93@gmail.com>
2020-12-31 22:49:42 +0900

    chore: test database conn

---
+++ SteamDataCollector/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SteamDataCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*-*-*-* Get Steam All Game Data *-*-*-*");
+
+            //DataBase.TestConnAsync();
+        }
+    }
+}
