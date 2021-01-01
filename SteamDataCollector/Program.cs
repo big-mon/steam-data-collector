00aa33ea42166c67f30a2fb3ffdf067dfded96d0
BIG-MON <mrmon93@gmail.com>
2021-01-01 16:49:45 +0900

    feat: add upsert sql

--- SteamDataCollector/Program.cs
+++ SteamDataCollector/Program.cs
@@ -1,14 +1,152 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text.Json;
+using SteamDataCollector.Models;
+using System.Linq;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using SteamDataCollector.Models.AppList;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using MySqlConnector;
 
 namespace SteamDataCollector
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>ストア地域</summary>
+        private enum CC
         {
-            Console.WriteLine("*-*-*-* Get Steam All Game Data *-*-*-*");
-
-            //DataBase.TestConnAsync();
+            /// <summary>アメリカ</summary>
+            us,
+            /// <summary>日本</summary>
+            jp
         }
+
+        private static async Task Main(string[] args)
+        {
+            // 全Appのリストを取得
+            List<int> list = await GetAppList();
+
+            // DBを更新
+            await UpdateData(list);
+        }
+
+        #region アプリ取得
+
+        /// <summary>
+        /// AppIDのリストを取得
+        /// </summary>
+        /// <returns>AppIDリスト</returns>
+        private static async Task<List<int>> GetAppList()
+        {
+            // Appリストを取得
+            var res = GetAllApps();
+            var appList = System.Text.Json.JsonSerializer.Deserialize<Root>(await res);
+
+            // IDリストを返却
+            return null != appList ? appList.Applist.Apps.Select(x => x.Appid).OrderBy(x => x).ToList() : new List<int>();
+        }
+
+        /// <summary>
+        /// Steamから全Appリストを取得
+        /// </summary>
+        /// <returns>API返却値</returns>
+        private static async Task<string> GetAllApps()
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json");
+
+            return result;
+        }
+
+        #endregion
+
+        #region DB更新
+
+        /// <summary>APIをコールしDBへ反映</summary>
+        /// <param name="ids">AppIDリスト</param>
+        private static async Task UpdateData(IReadOnlyList<int> ids)
+        {
+            var stopwatch = new Stopwatch();
+            var client = new HttpClient();
+
+            foreach (var id in ids)
+            {
+                foreach (var cc in Enum.GetValues(typeof(CC)))
+                {
+                    // 処理時間を計測開始
+                    stopwatch.Restart();
+
+                    // APIから結果取得
+                    var result = await client.GetStringAsync(string.Format("https://store.steampowered.com/api/appdetails/?l=jp&appids={0}&cc={1}", 728530, cc.ToString()));
+
+                    // オブジェクト変換
+                    var res = JObject.Parse(result).SelectToken("728530");
+                    var sa = new SteamApp(id.ToString(), res);
+
+                    // DB反映
+                    await UpdateDatabase(sa);
+
+                    // APIリミット回避のため待機
+                    stopwatch.Stop();
+                    WaitSleep((int)stopwatch.ElapsedMilliseconds);
+                }
+            }
+        }
+
+        /// <summary>DB更新</summary>
+        /// <param name="app">App情報</param>
+        private static async Task UpdateDatabase(SteamApp app)
+        {
+            var connString = $"Server={Properties.Resources.Server};Port={Properties.Resources.Port};Uid={Properties.Resources.UserID};Pwd={Properties.Resources.Password};Database={Properties.Resources.DataBase}";
+
+            using (var conn = new MySqlConnection(connString))
+            {
+                await conn.OpenAsync();
+
+                // apps
+                using (var cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO apps (`appid`, `name`, `type`, `recommendations`, `is_free`) VALUES (@appid, @name, @type, @recommendations, @is_free) ON DUPLICATE KEY UPDATE `name` = @name, `type` = @type, `recommendations` = @recommendations, `is_free` = @is_free, `update_time` = CURRENT_TIMESTAMP";
+                    cmd.Parameters.AddWithValue("appid", app.App.AppId);
+                    cmd.Parameters.AddWithValue("name", app.App.Name);
+                    cmd.Parameters.AddWithValue("type", app.App.Type);
+                    cmd.Parameters.AddWithValue("recommendations", app.App.Recommendations);
+                    cmd.Parameters.AddWithValue("is_free", app.App.IsFree);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                // developers
+                using (var cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO developers (`appid`, `name`, ) VALUES (@appid, @name) ON DUPLICATE KEY UPDATE `name` = @name, `update_time` = CURRENT_TIMESTAMP";
+                    cmd.Parameters.AddWithValue("appid", app.App.AppId);
+                    cmd.Parameters.AddWithValue("name", app.App.Name);
+                    _ = cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        /// <summary>指定時間を強制的に経過させる</summary>
+        /// <param name="elapsed">これまでの経過時間ミリ秒</param>
+        private static void WaitSleep(int elapsed)
+        {
+            var gap = int.Parse(Properties.Resources.SleepMilSec) - elapsed;
+            if (gap > 0) Thread.Sleep(gap);
+        }
+
+        #endregion
     }
 }
