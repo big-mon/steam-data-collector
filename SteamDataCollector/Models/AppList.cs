d3d464f13bb46036f9113c0e2b0c15453cca75ec
BIG-MON <mrmon93@gmail.com>
2021-01-01 16:46:55 +0900

    feat: add AppList

---
+++ SteamDataCollector/Models/AppList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SteamDataCollector.Models.AppList
+{
+    internal class Root
+    {
+        [JsonPropertyName("applist")]
+        public Applist Applist { get; set; }
+    }
+
+    internal class Applist
+    {
+        [JsonPropertyName("apps")]
+        public List<App> Apps { get; set; }
+    }
+
+    internal class App
+    {
+        [JsonPropertyName("appid")]
+        public int Appid { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
