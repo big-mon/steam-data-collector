9124e4fc2c509ba18e67d9d59116f563a9db969f
BIG-MON <mrmon93@gmail.com>
2021-01-02 14:50:19 +0900

    refactor: using句を簡略化

--- SteamDataCollector/DataBase.cs
+++ SteamDataCollector/DataBase.cs
@@ -42,68 +42,64 @@ namespace SteamDataCollector
         /// <param name="app">App情報</param>
         private static async Task UpdateApp(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
+
+            using var cmd = new MySqlCommand
             {
-                await conn.OpenAsync();
+                Connection = conn,
+                CommandText = "INSERT INTO apps (`appid`, `name`, `type`, `recommendations`, `is_free`) VALUES (@appid, @name, @type, @recommendations, @is_free) ON DUPLICATE KEY UPDATE `name` = @name, `type` = @type, `recommendations` = @recommendations, `is_free` = @is_free, `update_time` = CURRENT_TIMESTAMP"
+            };
+            cmd.Parameters.AddWithValue("appid", app.AppId);
 
-                using var cmd = new MySqlCommand
-                {
-                    Connection = conn,
-                    CommandText = "INSERT INTO apps (`appid`, `name`, `type`, `recommendations`, `is_free`) VALUES (@appid, @name, @type, @recommendations, @is_free) ON DUPLICATE KEY UPDATE `name` = @name, `type` = @type, `recommendations` = @recommendations, `is_free` = @is_free, `update_time` = CURRENT_TIMESTAMP"
-                };
-                cmd.Parameters.AddWithValue("appid", app.AppId);
-
-                if (app.IsSuccess)
-                {
-                    cmd.Parameters.AddWithValue("name", app.App.Name);
-                    cmd.Parameters.AddWithValue("type", app.App.Type);
-                    cmd.Parameters.AddWithValue("recommendations", app.App.Recommendations);
-                    cmd.Parameters.AddWithValue("is_free", app.App.IsFree);
-                }
-                else
-                {
-                    cmd.Parameters.AddWithValue("name", "");
-                    cmd.Parameters.AddWithValue("type", "");
-                    cmd.Parameters.AddWithValue("recommendations", 0);
-                    cmd.Parameters.AddWithValue("is_free", false);
-                }
-
-                await cmd.ExecuteNonQueryAsync();
+            if (app.IsSuccess)
+            {
+                cmd.Parameters.AddWithValue("name", app.App.Name);
+                cmd.Parameters.AddWithValue("type", app.App.Type);
+                cmd.Parameters.AddWithValue("recommendations", app.App.Recommendations);
+                cmd.Parameters.AddWithValue("is_free", app.App.IsFree);
             }
+            else
+            {
+                cmd.Parameters.AddWithValue("name", "");
+                cmd.Parameters.AddWithValue("type", "");
+                cmd.Parameters.AddWithValue("recommendations", 0);
+                cmd.Parameters.AddWithValue("is_free", false);
+            }
+
+            await cmd.ExecuteNonQueryAsync();
         }
 
         /// <summary>developersテーブルを更新</summary>
         /// <param name="app">App情報</param>
         private static async Task UpdateDevelopers(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
-            {
-                await conn.OpenAsync();
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
 
-                // クリーニング
-                using var cmd1 = new MySqlCommand
+            // クリーニング
+            using var cmd1 = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "DELETE FROM developers WHERE `appid` = @appid"
+            };
+            cmd1.Parameters.AddWithValue("appid", app.AppId);
+            await cmd1.ExecuteNonQueryAsync();
+
+            // 終了判定
+            if (null == app.App || null == app.App.Developers) return;
+
+            // 挿入
+            foreach (var item in app.App.Developers)
+            {
+                using var cmd2 = new MySqlCommand
                 {
                     Connection = conn,
-                    CommandText = "DELETE FROM developers WHERE `appid` = @appid"
+                    CommandText = "INSERT INTO developers (`appid`, `name`) VALUES (@appid, @name)"
                 };
-                cmd1.Parameters.AddWithValue("appid", app.AppId);
-                await cmd1.ExecuteNonQueryAsync();
-
-                // 終了判定
-                if (null == app.App || null == app.App.Developers) return;
-
-                // 挿入
-                foreach (var item in app.App.Developers)
-                {
-                    using var cmd2 = new MySqlCommand
-                    {
-                        Connection = conn,
-                        CommandText = "INSERT INTO developers (`appid`, `name`) VALUES (@appid, @name)"
-                    };
-                    cmd2.Parameters.AddWithValue("appid", app.AppId);
-                    cmd2.Parameters.AddWithValue("name", item.Name);
-                    _ = cmd2.ExecuteNonQueryAsync();
-                }
+                cmd2.Parameters.AddWithValue("appid", app.AppId);
+                cmd2.Parameters.AddWithValue("name", item.Name);
+                _ = cmd2.ExecuteNonQueryAsync();
             }
         }
 
@@ -111,34 +107,32 @@ namespace SteamDataCollector
         /// <param name="app">App情報</param>
         private static async Task UpdatePublishers(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
-            {
-                await conn.OpenAsync();
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
 
-                // クリーニング
-                using var cmd1 = new MySqlCommand
+            // クリーニング
+            using var cmd1 = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "DELETE FROM publishers WHERE `appid` = @appid"
+            };
+            cmd1.Parameters.AddWithValue("appid", app.AppId);
+            await cmd1.ExecuteNonQueryAsync();
+
+            // 終了判定
+            if (null == app.App || null == app.App.Publishers) return;
+
+            // 挿入
+            foreach (var item in app.App.Publishers)
+            {
+                using var cmd2 = new MySqlCommand
                 {
                     Connection = conn,
-                    CommandText = "DELETE FROM publishers WHERE `appid` = @appid"
+                    CommandText = "INSERT INTO publishers (`appid`, `name`) VALUES (@appid, @name)"
                 };
-                cmd1.Parameters.AddWithValue("appid", app.AppId);
-                await cmd1.ExecuteNonQueryAsync();
-
-                // 終了判定
-                if (null == app.App || null == app.App.Publishers) return;
-
-                // 挿入
-                foreach (var item in app.App.Publishers)
-                {
-                    using var cmd2 = new MySqlCommand
-                    {
-                        Connection = conn,
-                        CommandText = "INSERT INTO publishers (`appid`, `name`) VALUES (@appid, @name)"
-                    };
-                    cmd2.Parameters.AddWithValue("appid", app.AppId);
-                    cmd2.Parameters.AddWithValue("name", item.Name);
-                    _ = cmd2.ExecuteNonQueryAsync();
-                }
+                cmd2.Parameters.AddWithValue("appid", app.AppId);
+                cmd2.Parameters.AddWithValue("name", item.Name);
+                _ = cmd2.ExecuteNonQueryAsync();
             }
         }
 
@@ -146,35 +140,33 @@ namespace SteamDataCollector
         /// <param name="app">App情報</param>
         private static async Task UpdateGenres(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
-            {
-                await conn.OpenAsync();
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
 
-                // クリーニング
-                using var cmd1 = new MySqlCommand
+            // クリーニング
+            using var cmd1 = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "DELETE FROM genres WHERE `appid` = @appid"
+            };
+            cmd1.Parameters.AddWithValue("appid", app.AppId);
+            await cmd1.ExecuteNonQueryAsync();
+
+            // 終了判定
+            if (null == app.App || null == app.App.Genres) return;
+
+            // 挿入
+            foreach (var item in app.App.Genres)
+            {
+                using var cmd2 = new MySqlCommand
                 {
                     Connection = conn,
-                    CommandText = "DELETE FROM genres WHERE `appid` = @appid"
+                    CommandText = "INSERT INTO genres (`appid`, `name`, `id`) VALUES (@appid, @name, @id)"
                 };
-                cmd1.Parameters.AddWithValue("appid", app.AppId);
-                await cmd1.ExecuteNonQueryAsync();
-
-                // 終了判定
-                if (null == app.App || null == app.App.Genres) return;
-
-                // 挿入
-                foreach (var item in app.App.Genres)
-                {
-                    using var cmd2 = new MySqlCommand
-                    {
-                        Connection = conn,
-                        CommandText = "INSERT INTO genres (`appid`, `name`, `id`) VALUES (@appid, @name, @id)"
-                    };
-                    cmd2.Parameters.AddWithValue("appid", app.AppId);
-                    cmd2.Parameters.AddWithValue("name", item.Name);
-                    cmd2.Parameters.AddWithValue("id", item.Id);
-                    _ = cmd2.ExecuteNonQueryAsync();
-                }
+                cmd2.Parameters.AddWithValue("appid", app.AppId);
+                cmd2.Parameters.AddWithValue("name", item.Name);
+                cmd2.Parameters.AddWithValue("id", item.Id);
+                _ = cmd2.ExecuteNonQueryAsync();
             }
         }
 
@@ -182,34 +174,32 @@ namespace SteamDataCollector
         /// <param name="app">App情報</param>
         private static async Task UpdateLanguages(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
-            {
-                await conn.OpenAsync();
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
 
-                // クリーニング
-                using var cmd1 = new MySqlCommand
+            // クリーニング
+            using var cmd1 = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "DELETE FROM languages WHERE `appid` = @appid"
+            };
+            cmd1.Parameters.AddWithValue("appid", app.AppId);
+            await cmd1.ExecuteNonQueryAsync();
+
+            // 終了判定
+            if (null == app.App || null == app.App.Languages) return;
+
+            // 挿入
+            foreach (var item in app.App.Languages)
+            {
+                using var cmd2 = new MySqlCommand
                 {
                     Connection = conn,
-                    CommandText = "DELETE FROM languages WHERE `appid` = @appid"
+                    CommandText = "INSERT INTO languages (`appid`, `name`) VALUES (@appid, @name)"
                 };
-                cmd1.Parameters.AddWithValue("appid", app.AppId);
-                await cmd1.ExecuteNonQueryAsync();
-
-                // 終了判定
-                if (null == app.App || null == app.App.Languages) return;
-
-                // 挿入
-                foreach (var item in app.App.Languages)
-                {
-                    using var cmd2 = new MySqlCommand
-                    {
-                        Connection = conn,
-                        CommandText = "INSERT INTO languages (`appid`, `name`) VALUES (@appid, @name)"
-                    };
-                    cmd2.Parameters.AddWithValue("appid", app.AppId);
-                    cmd2.Parameters.AddWithValue("name", item);
-                    _ = cmd2.ExecuteNonQueryAsync();
-                }
+                cmd2.Parameters.AddWithValue("appid", app.AppId);
+                cmd2.Parameters.AddWithValue("name", item);
+                _ = cmd2.ExecuteNonQueryAsync();
             }
         }
 
@@ -217,69 +207,65 @@ namespace SteamDataCollector
         /// <param name="app">App情報</param>
         private static async Task UpdatePrices(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
+
+            // 終了判定
+            if (null == app.App || null == app.App.PriceOverview) return;
+
+            // クリーニング
+            using var cmd1 = new MySqlCommand
             {
-                await conn.OpenAsync();
+                Connection = conn,
+                CommandText = "DELETE FROM prices WHERE `appid` = @appid and `currency` = @currency"
+            };
+            cmd1.Parameters.AddWithValue("appid", app.AppId);
+            cmd1.Parameters.AddWithValue("currency", null == app.App.PriceOverview.Currency);
+            await cmd1.ExecuteNonQueryAsync();
 
-                // 終了判定
-                if (null == app.App || null == app.App.PriceOverview) return;
-
-                // クリーニング
-                using var cmd1 = new MySqlCommand
-                {
-                    Connection = conn,
-                    CommandText = "DELETE FROM prices WHERE `appid` = @appid and `currency` = @currency"
-                };
-                cmd1.Parameters.AddWithValue("appid", app.AppId);
-                cmd1.Parameters.AddWithValue("currency", null == app.App.PriceOverview.Currency);
-                await cmd1.ExecuteNonQueryAsync();
-
-                // 挿入
-                using var cmd2 = new MySqlCommand
-                {
-                    Connection = conn,
-                    CommandText = "INSERT INTO prices (`appid`, `currency`, `initial`, `final`, `discount_percent`) VALUES (@appid, @currency, @initial, @final, @discount_percent)"
-                };
-                cmd2.Parameters.AddWithValue("appid", app.AppId);
-                cmd2.Parameters.AddWithValue("currency", app.App.PriceOverview.Currency);
-                cmd2.Parameters.AddWithValue("initial", app.App.PriceOverview.Initial);
-                cmd2.Parameters.AddWithValue("final", app.App.PriceOverview.Final);
-                cmd2.Parameters.AddWithValue("discount_percent", app.App.PriceOverview.DiscountPercent);
-                _ = cmd2.ExecuteNonQueryAsync();
-            }
+            // 挿入
+            using var cmd2 = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "INSERT INTO prices (`appid`, `currency`, `initial`, `final`, `discount_percent`) VALUES (@appid, @currency, @initial, @final, @discount_percent)"
+            };
+            cmd2.Parameters.AddWithValue("appid", app.AppId);
+            cmd2.Parameters.AddWithValue("currency", app.App.PriceOverview.Currency);
+            cmd2.Parameters.AddWithValue("initial", app.App.PriceOverview.Initial);
+            cmd2.Parameters.AddWithValue("final", app.App.PriceOverview.Final);
+            cmd2.Parameters.AddWithValue("discount_percent", app.App.PriceOverview.DiscountPercent);
+            _ = cmd2.ExecuteNonQueryAsync();
         }
 
         /// <summary>releasesテーブルを更新</summary>
         /// <param name="app">App情報</param>
         private static async Task UpdateReleases(SteamApp app)
         {
-            using (var conn = new MySqlConnection(StoreAPI.ConnString))
+            using var conn = new MySqlConnection(StoreAPI.ConnString);
+            await conn.OpenAsync();
+
+            // クリーニング
+            using var cmd1 = new MySqlCommand
             {
-                await conn.OpenAsync();
+                Connection = conn,
+                CommandText = "DELETE FROM releases WHERE `appid` = @appid"
+            };
+            cmd1.Parameters.AddWithValue("appid", app.AppId);
+            await cmd1.ExecuteNonQueryAsync();
 
-                // クリーニング
-                using var cmd1 = new MySqlCommand
-                {
-                    Connection = conn,
-                    CommandText = "DELETE FROM releases WHERE `appid` = @appid"
-                };
-                cmd1.Parameters.AddWithValue("appid", app.AppId);
-                await cmd1.ExecuteNonQueryAsync();
+            // 終了判定
+            if (null == app.App || null == app.App.Release) return;
 
-                // 終了判定
-                if (null == app.App || null == app.App.Release) return;
-
-                // 挿入
-                using var cmd2 = new MySqlCommand
-                {
-                    Connection = conn,
-                    CommandText = "INSERT INTO releases (`appid`, `comming_soon`, `date`) VALUES (@appid, @comming_soon, @date)"
-                };
-                cmd2.Parameters.AddWithValue("appid", app.AppId);
-                cmd2.Parameters.AddWithValue("comming_soon", app.App.Release.IsUnRelease);
-                cmd2.Parameters.AddWithValue("date", app.App.Release.Date);
-                _ = cmd2.ExecuteNonQueryAsync();
-            }
+            // 挿入
+            using var cmd2 = new MySqlCommand
+            {
+                Connection = conn,
+                CommandText = "INSERT INTO releases (`appid`, `comming_soon`, `date`) VALUES (@appid, @comming_soon, @date)"
+            };
+            cmd2.Parameters.AddWithValue("appid", app.AppId);
+            cmd2.Parameters.AddWithValue("comming_soon", app.App.Release.IsUnRelease);
+            cmd2.Parameters.AddWithValue("date", app.App.Release.Date);
+            _ = cmd2.ExecuteNonQueryAsync();
         }
     }
 }
