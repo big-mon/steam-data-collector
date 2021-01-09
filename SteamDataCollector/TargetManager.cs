cf374ceb48f0004889c3d09356b9d9c68f6b09b0
BIG-MON <mrmon93@gmail.com>
2021-01-09 12:59:47 +0900

    refactor: メソッド名変更

--- SteamDataCollector/TargetManager.cs
+++ SteamDataCollector/TargetManager.cs
@@ -12,38 +12,41 @@ namespace SteamDataCollector
     /// <summary>アプリ取得</summary>
     internal class TargetManager
     {
+        /// <summary>SteamからAppリストを取得</summary>
+        /// <param name="url">APIのURL</param>
+        /// <returns>API返却値</returns>
+        private static async Task<string> RequestAPI(string url)
+        {
+            var client = new HttpClient();
+            string result = await client.GetStringAsync(url);
+
+            return result;
+        }
+
+        #region AllApps
+
         /// <summary>AppIDのリストを取得</summary>
         /// <returns>AppIDリスト</returns>
-        internal static async Task<List<string>> GetTargetList()
+        internal static async Task<List<string>> FetchAllAppsList()
         {
             // Appリストを取得
-            Task<List<string>> originList = GetAllAppList();
+            Task<List<string>> originList = NormalizeApiToList();
 
             // 除外リストを取得
-            Task<List<string>> rejectList = GetRejectList();
+            Task<List<string>> rejectList = RetrivetRejectList();
 
             // Appリストへ除外リストを適用
-            List<string> list = GetFormattedTargetList(await originList, await rejectList);
+            List<string> list = IgnoreDuplicatedID(await originList, await rejectList);
 
             // IDリストを返却
             return list;
         }
 
-        /// <summary>Steamから全Appリストを取得</summary>
-        /// <returns>API返却値</returns>
-        private static async Task<string> GetAllApps()
-        {
-            var client = new HttpClient();
-            string result = await client.GetStringAsync(StoreAPI.AllAppURL);
-
-            return result;
-        }
-
         /// <summary>API返却値をリストに変換</summary>
         /// <returns>AppIDリスト</returns>
-        private static async Task<List<string>> GetAllAppList()
+        private static async Task<List<string>> NormalizeApiToList()
         {
-            var appList = JsonSerializer.Deserialize<Root>(await GetAllApps());
+            var appList = JsonSerializer.Deserialize<Root>(await RequestAPI(StoreAPI.AllAppURL));
             var resList = null != appList ? appList.Applist.Apps.Select(x => x.Appid.ToString()).OrderByDescending(x => int.Parse(x)).ToList() : new List<string>();
 
             return resList;
@@ -51,7 +54,7 @@ namespace SteamDataCollector
 
         /// <summary>除外対象リストを取得</summary>
         /// <returns>除外対象リスト</returns>
-        private static async Task<List<string>> GetRejectList()
+        private static async Task<List<string>> RetrivetRejectList()
         {
             var resList = new List<string>();
 
@@ -76,12 +79,14 @@ namespace SteamDataCollector
         /// <param name="origin">全Appリスト</param>
         /// <param name="reject">除外リスト</param>
         /// <returns>除外後リスト</returns>
-        private static List<string> GetFormattedTargetList(IReadOnlyList<string> origin, IReadOnlyList<string> reject)
+        private static List<string> IgnoreDuplicatedID(IReadOnlyList<string> origin, IReadOnlyList<string> reject)
         {
             HashSet<string> rejectIDs = new HashSet<string>(reject);
             var resList = origin.Where(x => !rejectIDs.Contains(x)).ToList();
 
             return resList;
         }
+
+        #endregion AllApps
     }
 }
